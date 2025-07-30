96ebb3ed6ab7cf2e42ba64d27850698ac4586246
ratul0407 <rajaulislamratul12@gmail.com>
2025-07-30 14:34:43 +0600

    created parcel interface

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import router from "./app/routes";
+import cookieParser from "cookie-parser";
 import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
 const app = express();
+app.use(cookieParser());
 app.use(express.json());
 app.use(cors());
 
---
+++ src/app/modules/parcel/parcel.interface.ts
@@ -0,0 +1,35 @@
+import { Types } from "mongoose";
+import { IAddress } from "../user/user.interface";
+
+export enum Status {
+  REQUESTED = "REQUESTED",
+  APPROVED = "APPROVED",
+  PENDING_PICKUP = "PENDING_PICKUP",
+  PICKED_UP = "PICKED_UP",
+  IN_TRANSIT = "IN_TRANSIT",
+  OUT_FOR_DELIVERY = "OUT_FOR_DELIVERY",
+  DELIVERED = "DELIVERED",
+  CANCELLED = "CANCELLED",
+  FAILED_DELIVERY = "FAILED_DELIVERY",
+  RESCHEDULED = "RESCHEDULED",
+}
+export interface IStatus {
+  status: Status;
+  at: Date;
+}
+export interface IParcel {
+  _id?: string;
+  trackingId: string;
+  name: string;
+  senderLocation: IAddress;
+  receiverLocation: IAddress;
+  sameDivision: boolean;
+  senderEmail: string;
+  receiverEmail: string;
+  trackingEvents: IStatus[];
+  weight: number;
+  estimatedDeliveryDate: Date;
+  pickUpDate?: Date;
+  deliveryDate?: Date;
+  deliveryDriver?: Types.ObjectId;
+}
